7f4817b7de647e3de73297b48e261be23240c4fa
RihenUniverse <rihen.universe@gmail.com>
2026-05-15 12:28:58 +0100

    fix: correction casse Core, Utils, Commands + suppression fichiers temporaires

--- tmp_nkplatform_pch_test.o.d
+++
@@ -1,4 +0,0 @@
-e:\Projets\MacShared\Projets\Jenga\tmp_nkplatform_pch_test.o: \
-  E:/Projets/MacShared/Projets/Jenga/Jenga/Exemples/Nkentseu/Modules/Foundation/NKCore/pch/pch.h \
-  e:/Projets/MacShared/Projets/Jenga/tmp_nkplatform_pch_test.cpp \
-  e:/Projets/MacShared/Projets/Jenga/Jenga/Exemples/Nkentseu/Modules/Foundation/NKPlatform/src/NKPlatform/NkFoundationLog.h
--- tmp_va_android_test5.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdarg.h>
-#include <stdio.h>
-inline void f(const char* fmt, ...) {
-    std::__va_list args;
-    __builtin_va_start(args, fmt);
-    (void)vsnprintf(nullptr, 0, fmt, args);
-    __builtin_va_end(args);
-}
-int g(){ f("%d",1); return 0; }
